c3e394f348a8d5aaf3bd5c06a2d8e6df83eb58f6
Ayudesee <sharaevvlad@mail.ru>
2021-11-24 22:06:57 +0300

    halftime align height

--- imageCollector.py
+++ imageCollector.py
@@ -45,10 +45,10 @@ def collect_image(request_json, stat_json):
         for idx_team, team in enumerate(round['teams']):
             if isOvertimeinGame:
                 halftimes = f"{team['team_stats']['First Half Score']}—{team['team_stats']['Second Half Score']}—{team['team_stats']['Overtime score']}"
-                draw.text((25, 235 + 50 * idx_team), halftimes, font=font_halftimes)
+                draw.text((25, 235 + 42 * idx_team), halftimes, font=font_halftimes)
             else:
                 halftimes = f"{team['team_stats']['First Half Score']}—{team['team_stats']['Second Half Score']}"
-                draw.text((45, 235 + 50 * idx_team), halftimes, font=font_halftimes)
+                draw.text((45, 235 + 42 * idx_team), halftimes, font=font_halftimes)
             for idx_player, player in enumerate(team['players']):
                 if request_json['payload']['teams'][idx_team]['roster'][idx_player]['avatar'] != '':
                     avatar_req = requests.get(request_json['payload']['teams'][idx_team]['roster'][idx_player]['avatar'], stream=True)
