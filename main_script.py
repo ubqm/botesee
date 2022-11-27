dfe8c2e0a865fbd82438d3bfbcd1ea5ef1e09d96
Ayudesee <sharaevvlad@mail.ru>
2022-11-27 20:42:15 +0300

    async flask

--- main_script.py
+++ main_script.py
@@ -1,7 +1,6 @@
 import discord
 import os
 import flask
-from IPython.terminal.pt_inputhooks.asyncio import loop
 from flask import Flask, request, Response
 from threading import Thread
 from functools import partial
@@ -24,30 +23,45 @@ async def respond_default_get():
 
 
 @app.route("/match_status_ready", methods=["POST"])
-def respond_status_ready():
+async def respond_status_ready():
     print(request.json)
-    loop.create_task(bot_client.post_faceit_message_ready(
-        channel_id=828940900033626113, request_json=request.json))
+    await bot_client.post_faceit_message_ready(
+        channel_id=828940900033626113, request_json=request.json)
     print("respond match_status_ready function called")
     return Response(status=200)
 
 
 @app.route("/match_status_finished", methods=["POST"])
-def respond_status_finished():
+async def respond_status_finished():
     print(request.json)
-    loop.create_task(bot_client.post_faceit_message_finished(
-        channel_id=828940900033626113, request_json=request.json))
+    await bot_client.post_faceit_message_finished(
+        channel_id=828940900033626113, request_json=request.json)
     print("respond match_status_finished function called")
     return Response(status=200)
 
 
 @app.route("/match_status_aborted", methods=["POST"])
-def respond_status_aborted():
+async def respond_status_aborted():
     print(request.json)
-    loop.create_task(bot_client.post_faceit_message_aborted(
-        channel_id=828940900033626113, request_json=request.json))
+    await bot_client.post_faceit_message_aborted(
+        channel_id=828940900033626113, request_json=request.json)
     print("respond match_status_aborted function called")
     return Response(status=200)
+
+
+async def main():
+    intents = discord.Intents.all()
+    bot_client = MyDiscordClient(intents=intents)
+
+    port = int(os.environ.get("PORT", 5000))
+    print(f"variable port:{port}")
+    partial_run = partial(app.run, host="0.0.0.0", port=port)
+
+    await init()
+    t1 = Thread(target=partial_run, daemon=False)
+    t1.start()
+
+    await bot_client.start(discord_token)
 
 
 if __name__ == "__main__":
@@ -58,7 +72,8 @@ if __name__ == "__main__":
     print(f"variable port:{port}")
     partial_run = partial(app.run, host="0.0.0.0", port=port)
 
-    t = Thread(target=partial_run)
-    t.start()
     run_async(init())
+    t1 = Thread(target=partial_run, daemon=False)
+    t1.start()
+
     bot_client.run(discord_token)
