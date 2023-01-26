bc2e64b352bfc189fe2a44a911af563fd3fcfb45
Ayudesee <sharaevvlad@mail.ru>
2023-01-26 19:03:18 +0300

    Many updates before refactor

--- main_script.py
+++ main_script.py
@@ -22,29 +22,38 @@ async def main():
     async def respond_default_get():
         player_data, matches_data, elo_data = await db_fetch_data()
         context = {"players": player_data, "matches": matches_data, "elo": elo_data}
-        return flask.render_template('index.html', **context)
+        return flask.render_template("index.html", **context)
 
     @app.route("/match_status_ready", methods=["POST"])
     async def respond_status_ready():
         print(request.json)
-        loop.create_task(bot_client.post_faceit_message_ready(
-            channel_id=828940900033626113, request_json=request.json))
+        loop.create_task(
+            bot_client.post_faceit_message_ready(
+                channel_id=828940900033626113, request_json=request.json
+            )
+        )
         print("respond match_status_ready function called")
         return Response(status=200)
 
     @app.route("/match_status_finished", methods=["POST"])
     async def respond_status_finished():
         print(request.json)
-        loop.create_task(bot_client.post_faceit_message_finished(
-            channel_id=828940900033626113, request_json=request.json))
+        loop.create_task(
+            bot_client.post_faceit_message_finished(
+                channel_id=828940900033626113, request_json=request.json
+            )
+        )
         print("respond match_status_finished function called")
         return Response(status=200)
 
     @app.route("/match_status_aborted", methods=["POST"])
     async def respond_status_aborted():
         print(request.json)
-        loop.create_task(bot_client.post_faceit_message_aborted(
-            channel_id=828940900033626113, request_json=request.json))
+        loop.create_task(
+            bot_client.post_faceit_message_aborted(
+                channel_id=828940900033626113, request_json=request.json
+            )
+        )
         print("respond match_status_aborted function called")
         return Response(status=200)
 
