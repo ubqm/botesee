36c923d879e6e17cc69d74f48ff752118c872479
Ayudesee <sharaevvlad@mail.ru>
2023-10-23 15:55:32 +0300

    Dropped relationship

--- bot/db/models/gambling.py
+++ bot/db/models/gambling.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from decimal import Decimal
 from enum import StrEnum
-from typing import TYPE_CHECKING
 from uuid import UUID, uuid4
 
 from sqlalchemy import DateTime, ForeignKey, Integer, String, func, text
@@ -11,9 +10,6 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.types import DECIMAL
 
 from bot.db.models.base import Base
-
-if TYPE_CHECKING:
-    pass
 
 
 class TransactionEvent(StrEnum):
