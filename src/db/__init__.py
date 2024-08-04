081359a0ea7ef3d01b471f81fbce6a80e634286c
ubiqum <sharaevvlad@mail.ru>
2024-08-04 11:59:11 +0300

    Rename Session to session_maker

--- src/db/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.db.models.elo import Elo
-from src.db.models.match import Match
-from src.db.models.player import Player
-from src.db.models.base import Session, engine, Base
-from src.db.models.gambling import BetMatch, BetEvent, BetCoefficient, BetTransactions
