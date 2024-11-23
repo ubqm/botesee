e3c01078a5db3b4242e96ac6d11530da89855c8e
ubiqum <sharaevvlad@mail.ru>
2024-11-24 00:42:21 +0300

    RabbitMQ improvements

--- src/web/dependencies.py
+++ src/web/dependencies.py
@@ -1,6 +1,11 @@
+import asyncio.events
+
 from src import conf
 from src.clients.rabbit import RabbitClient
 
 
 async def get_rabbit() -> RabbitClient:
-    return RabbitClient(conf.RABBIT_HOST, conf.RABBIT_PORT, conf.RABBIT_USER, conf.RABBIT_PASSWORD)
+    loop = asyncio.events.get_running_loop()
+    return RabbitClient(
+        conf.RABBIT_HOST, conf.RABBIT_PORT, conf.RABBIT_USER, conf.RABBIT_PASSWORD, loop
+    )
