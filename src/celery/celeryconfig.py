ebd6567afe017f8d8b5119a6ff9c0e5b93b17f54
ubqm <sharaevvlad@mail.ru>
2025-03-15 13:06:15 +0300

    [fix] name of weekly stats

--- src/celery/celeryconfig.py
+++ src/celery/celeryconfig.py
@@ -3,8 +3,8 @@ from celery.schedules import crontab
 celery_config = {
     "timezone": "Europe/Minsk",
     "beat_schedule": {
-        "src.celery.tasks.weekly_stats": {
-            "task": "src.celery.tasks.weekly_stats",
+        "weekly_stats": {
+            "task": "weekly_stats",
             "schedule": crontab(hour="23", minute="59", day_of_week="sun"),
         },
     },
